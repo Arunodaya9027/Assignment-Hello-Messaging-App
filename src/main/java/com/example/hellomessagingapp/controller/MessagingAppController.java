7ab48fa009d3bcc6d31b6f6f813bbc3a730fe9c8
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-03-02 20:07:53 +0530

    [Arunodaya Pratap Singh] add. Implement GET REST API with Query Parameter

--- src/main/java/com/example/hellomessagingapp/controller/MessagingAppController.java
+++ src/main/java/com/example/hellomessagingapp/controller/MessagingAppController.java
@@ -1,6 +1,8 @@
 package com.example.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class MessagingAppController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/query")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
     }
 }
